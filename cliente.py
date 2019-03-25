e4a3062a327e2e7f5a505515cea3bc13b168d272
LuisValverde1296 <luis.valverdecruz@ucr.ac.cr>
2019-03-24 21:25:00 -0600

    Modificaciones a cliente.py que de momento no funcionan.

--- cliente.py
+++ cliente.py
@@ -3,14 +3,15 @@
 import socket
 from collections import deque
 import time
+import threading
 
 TCP_IP = '10.1.138.31'
 TCP_PORT = 5020
 BUFFER_SIZE = 1024
 #MESSAGE = "Hello, World!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((TCP_IP, TCP_PORT))
 
 ######################################################################################
 #esto esta en cliente
@@ -34,20 +35,28 @@ s.connect((TCP_IP, TCP_PORT))
 #print(cola)
 #####################################################################################
 
-num=input('Introduzca la espera en segundos: \n')
-#reviso que ingreso sea un numero
-try:
-	val=int(num)
-	tiempo = val
-except:
-	print("Eso no es un numero, se pondran 5 segundos")
-	tiempo = 5
-message = ''
-while message != '1':
-	message = input('Digite su oracion\n')
-	#mes = list(message)
-	s.send(message.encode())
-	time.sleep(tiempo);
-data = s.recv(BUFFER_SIZE)
-s.close()
-print (data.decode())
+def lector():
+	num=input('Introduzca la espera en segundos: \n')
+	#reviso que ingreso sea un numero
+	try:
+		val=int(num)
+		tiempo = val
+	except:
+		print("Eso no es un numero, se pondran 5 segundos")
+		tiempo = 5
+	message = ''
+	cola = queue()
+	#cola_2 = Queue()
+	while message != '1':
+		message = input('Digite su oracion\n')
+		print(message)
+		#mes = list(message)
+		cola.put(message)
+		#s.send(message.encode())
+		#data = s.recv(BUFFER_SIZE)
+		#cola_2.put(data.decode())	
+		time.sleep(tiempo)
+		print(cola)
+	#s.close()
+	
+lector()
