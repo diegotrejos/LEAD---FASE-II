65579fa637e2e4ead48194e8ba071dfba3604404
root <urenaesteban27@gmail.com>
2019-03-25 21:29:59 -0700

    Cambios para que sirva con semaforos

--- cliente.py
+++ cliente.py
@@ -4,57 +4,7 @@ import queue
 import time
 import threading
 import pair
-import asyncio
 
-TCP_IP = '192.168.0.6'
-TCP_PORT = 5000
-BUFFER_SIZE = 1024
-
-
-mensaje = ""
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((TCP_IP, TCP_PORT))
-
-
-cola = queue.Queue()
-semaforo1 = asyncio.Semaphore(0)
-lista_resultados = []
-counter = 0
-
-def lector():
-	num=input('Introduzca la espera en segundos: \n')
-	#reviso que ingreso sea un numero
-	try:
-		val=int(num)
-		tiempo = val
-	except:
-		print("Eso no es un numero, se pondran 5 segundos")
-		tiempo = 5
-	
-	while mensaje != "1":
-		mensaje = input('Digite su oracion\n')
-		#print(mensaje)
-		myPair = pair.Pair(mensaje)
-		listaResultados.append(myPair)
-		cola.put(myPair)
-		semaforo1.release()
-		
-	
-
-
-def enviar():
-	seguir = True
-	while seguir:
-		semaforo1.acquire()
-		mensj = cola.get().oracion
-		if mensj == "1":
-			seguir = False
-		socket.send(mensj.encode())
-		data = socket.recv(BUFFER_SIZE)
-		list[counter].cantidadPalabras = data.decode()
-		counter = counter + 1
-		
 class MyThread (threading.Thread):
 	def __init__(self, threadID): #override del constructor
 		threading.Thread.__init__(self)
@@ -69,12 +19,77 @@ class MyThread (threading.Thread):
 			# Envie
 			print("Soy el Thread 1")
 			enviar()
+
+TCP_IP = '192.168.205.131'
+TCP_PORT = 5050
+BUFFER_SIZE = 1024
+
+#socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#socket.connect((TCP_IP, TCP_PORT))
+
+
+cola = queue.Queue()
+listaResultados = []
+
+sem1 = threading.Semaphore(0) #semaforo 1
+sem2 = threading.Semaphore(0) #semafoto 2
+
+
+
+def imprimirResultados():
+	for pair in listaResultados:
+		print(str(pair.oracion) + ": " + str(pair.cantidadPalabras))
+
+
+
+def lector():
+	num=input('Introduzca la espera en segundos: \n')
+	#reviso que ingreso sea un numero
+	try:
+		val=int(num)
+		tiempo = val
+	except:
+		print("Eso no es un numero, se pondran 5 segundos")
+		tiempo = 5
+	
+	mensaje = " "
+	
+	while mensaje != "1":
+		mensaje = input('Digite su oracion\n')
+		#print(mensaje)
+		myPair = pair.Pair(mensaje)
+		listaResultados.append(myPair)
+		cola.put(myPair)
+		sem1.release()
+	# Aqui sigue cuando lo que se encuentra es un 1
+	print("Soy el thread 0 y estoy esperando por el queue")
+	sem2.acquire()
+	imprimirResultados()
+
+def enviar():
+	global contador
+	seguir = True
+	while seguir:
+		sem1.acquire()
+		while cola.empty() == False:
+			mensj = cola.get().oracion
+			if mensj == "1":
+				seguir = False
+				sem2.release()
+				print("Release del semaforo 2")
+			#socket.send(mensj.encode())
+			#data = socket.recv(BUFFER_SIZE)
+			#list[contador].cantidadPalabras = data.decode()
+			contador += 1
+			sem1.release()			
+
 			
-			
+contador = 0
+
 thread1 = MyThread(0)
 thread2 = MyThread(1)
 thread1.start()
 thread2.start()
 thread1.join()
 thread2.join()
-socket.close()
+#socket.close()
